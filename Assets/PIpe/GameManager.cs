9dc0ce35cc5aa5381d25db68f43682aa30417660
green-Desktop <appieosugood@gmail.com>
2023-08-22 00:46:39 +0800

    water color work, and random pipe dir

--- Assets/PIpe/GameManager.cs
+++ Assets/PIpe/GameManager.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections.Generic;
 using GameSetting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Pipe
 {
+    internal enum GameFlow
+    {
+        START,
+        PLAYING,
+        WIN
+    }
+
     public class GameManager : MonoBehaviour
     {
         [SerializeField] private PipeData pipeData;
@@ -11,7 +20,10 @@ namespace Pipe
         private readonly List<Vector2> iToV2 = new();
         private readonly Dictionary<Vector2, int> V2ToIndex = new();
         private Camera _camera;
+        private GameFlow _gameFlow;
         private List<List<UnitPipe>> _pipe2D;
+        private List<List<UnitPipeGameObject>> _pipeGameObjects;
+        private Vector2 _waterSource;
 
         private void Awake()
         {
@@ -29,6 +41,7 @@ namespace Pipe
         {
             _camera = Camera.main;
             // _camera.
+            _pipeGameObjects = new List<List<UnitPipeGameObject>>();
             _pipe2D = new List<List<UnitPipe>>();
             for (var y = 0; y < pipeData.mapSize.y; y++)
             {
@@ -40,21 +53,48 @@ namespace Pipe
             var init = new Vector2(Random.Range(0, (int)pipeData.mapSize.x - 1),
                 Random.Range(0, (int)pipeData.mapSize.y - 1));
             _pipe2D = GenerateMap(_pipe2D, init, pipeData.puzzleType);
+            _waterSource = init;
 
             for (var y = 0; y < _pipe2D.Count; y++)
             {
                 var pipe1D = _pipe2D[y];
+                var list = new List<UnitPipeGameObject>();
                 for (var x = 0; x < pipe1D.Count; x++)
                 {
                     var p = pipe1D[x];
                     var np = Instantiate(pipePrefab, transform);
-                    np.transform.name = "pipe" + y + x;
+                    np.transform.name = "pipe" + y + "-" + x;
                     np.transform.position =
                         GetPipePositon(pipeData.boardLeftDown, pipeData.pipeSize, new Vector3(x, y, 0));
                     var unitPipeGameObject = np.GetComponent<UnitPipeGameObject>();
+                    unitPipeGameObject.SetGameManager(this);
                     unitPipeGameObject.SetPuzzleType(pipeData.puzzleType);
                     unitPipeGameObject.SetUnitPipe(p);
+                    var ran = Random.Range(0, pipeData.puzzleType);
+                    for (var i = 0; i < ran; i++) unitPipeGameObject.RotateOverClock(true);
+                    list.Add(unitPipeGameObject);
                 }
+
+                _pipeGameObjects.Add(list);
+            }
+
+            _gameFlow = GameFlow.START;
+        }
+
+        private void Update()
+        {
+            switch (_gameFlow)
+            {
+                case GameFlow.START:
+                    UpdatePipe();
+                    _gameFlow = GameFlow.PLAYING;
+                    break;
+                case GameFlow.PLAYING:
+                    break;
+                case GameFlow.WIN:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
@@ -129,6 +169,53 @@ namespace Pipe
         private T Get2DArrByVector2<T>(List<List<T>> pipe2D, Vector2 now)
         {
             return pipe2D[(int)now.y][(int)now.x];
+        }
+
+        public void UpdatePipe()
+        {
+            var waterPipe = GetWaterPipe(_pipe2D, _waterSource, pipeData.puzzleType);
+            for (var y = 0; y < waterPipe.Count; y++)
+            for (var x = 0; x < waterPipe[0].Count; x++)
+                Get2DArrByVector2(_pipeGameObjects, new Vector2(x, y)).SetConnectWaterSource(waterPipe[y][x]);
+        }
+
+        private List<List<bool>> GetWaterPipe(List<List<UnitPipe>> pipe2D, Vector2 waterSource, int puzzleType)
+        {
+            var visted = new List<Vector2>();
+            var candidate = new Queue<Vector2>();
+            candidate.Enqueue(waterSource);
+            while (candidate.Count != 0)
+            {
+                var now = candidate.Dequeue();
+                visted.Add(now);
+                var linkState = Get2DArrByVector2(pipe2D, now).connections;
+                for (var dir = 0; dir < linkState.Count; dir++)
+                {
+                    var next = now + iToV2[dir];
+                    if (!InMap(next, new Vector2(pipe2D[0].Count, pipe2D.Count))
+                        || visted.Contains(next))
+                        continue;
+                    if (linkState[dir] && Get2DArrByVector2(pipe2D, now + iToV2[dir])
+                            .connections[(dir + puzzleType / 2) % puzzleType]) candidate.Enqueue(next);
+                }
+            }
+
+            List<List<bool>> waterPipe = new();
+            for (var y = 0; y < pipe2D[0].Count; y++)
+            {
+                var pipe1D = new List<bool>();
+                for (var x = 0; x < pipe2D.Count; x++) pipe1D.Add(false);
+                waterPipe.Add(pipe1D);
+            }
+
+            foreach (var vector2 in visted) Set2DArrByVector2(waterPipe, vector2, true);
+
+            return waterPipe;
+        }
+
+        private void Set2DArrByVector2<T>(List<List<T>> arr2, Vector2 vector2, T t)
+        {
+            arr2[(int)vector2.y][(int)vector2.x] = t;
         }
     }
 }
--- Assets/PIpe/UnitPipeGameObject.cs
+++ Assets/PIpe/UnitPipeGameObject.cs
@@ -6,6 +6,8 @@ namespace Pipe
 {
     public class UnitPipeGameObject : MonoBehaviour
     {
+        private List<SpriteRenderer> _childSprites;
+        private GameManager _gameManager;
         private UnitPipe _originUnitPipe;
         private GameObject _pipeEnd;
         private List<GameObject> _pipeLine;
@@ -24,12 +26,16 @@ namespace Pipe
         {
             var angle = 360 / _puzzleType;
             _pipeEnd.SetActive(_originUnitPipe.GetNumOfConnection() == 1);
+            _childSprites = new List<SpriteRenderer>();
             for (var i = 1; i < _puzzleType; i++)
             {
                 var line = Instantiate(transform.GetChild(1).GameObject(), transform);
                 line.transform.Rotate(new Vector3(0, 0, angle * i));
                 _pipeLine.Add(line);
             }
+
+            for (var i = 0; i < transform.childCount; i++)
+                _childSprites.Add(transform.GetChild(i).transform.GetChild(0).GetComponent<SpriteRenderer>());
 
             var index = 1;
             foreach (var up in _originUnitPipe.connections) transform.GetChild(index++).GameObject().SetActive(up);
@@ -39,7 +45,18 @@ namespace Pipe
         {
             rotationTimes = (rotationTimes + 1) % _puzzleType;
             transform.Rotate(new Vector3(0, 0, 90));
-            _originUnitPipe.RotateOverClock(true);
+            RotateOverClock(true);
+            _gameManager.UpdatePipe();
+        }
+
+        public void RotateOverClock(bool b)
+        {
+            _originUnitPipe.RotateOverClock(b);
+        }
+
+        public void SetGameManager(GameManager gameManager)
+        {
+            _gameManager = gameManager;
         }
 
         public void SetPuzzleType(int line)
@@ -50,6 +67,15 @@ namespace Pipe
         public void SetUnitPipe(UnitPipe up)
         {
             _originUnitPipe = up;
+        }
+
+        public void SetConnectWaterSource(bool b)
+        {
+            foreach (var sprite in _childSprites)
+                if (b)
+                    sprite.color = Color.blue;
+                else
+                    sprite.color = Color.black;
         }
     }
 }
